b4b81eb610d7033acb935f9fb64ff589b97d8059
Moien Tajik <moientajik@yahoo.com>
2019-02-08 13:25:58 +0330

    Replaced direct mappings with AutoMapper

---
+++ Pangul/Infrastructure/Mappings/DomainProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Pangul.Services;
+using System;
+using System.Net.Http;
+
+namespace Pangul.Infrastructure.Mappings
+{
+    public class DomainProfile : Profile
+    {
+        public DomainProfile()
+        {
+            CreateMap<HttpResponseMessage, HealthStatus>()
+                .ConvertUsing<HttpResponseMessageToHealthStatusConverter>();
+        }
+    }
+
+    public class HttpResponseMessageToHealthStatusConverter : ITypeConverter<HttpResponseMessage, HealthStatus>
+    {
+        public HealthStatus Convert(HttpResponseMessage httpReponseMessage, HealthStatus destination, ResolutionContext context)
+        {
+            string url = context.Items["url"].ToString();
+            bool isDown = httpReponseMessage == null;
+            int? statusCode = (int?)httpReponseMessage?.StatusCode;
+            DateTime requestTime = DateTime.Now;
+
+            return new HealthStatus(url, isDown, statusCode, requestTime);
+        }
+    }
+}
--- Pangul/Startup.cs
+++ Pangul/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Pangul.Services;
+using System.Collections.Generic;
 
 namespace Pangul
 {
@@ -18,23 +19,20 @@ namespace Pangul
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddAutoMapper();
 
             AddCustomServices(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -44,7 +42,6 @@ namespace Pangul
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
